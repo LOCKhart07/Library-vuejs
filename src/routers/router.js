4679a6b144426fb483ff24047175aeb97ef8743d
Jenslee Dsouza <dsouzajenslee@gmail.com>
2023-03-07 15:42:36 +0530

    removed edit book route

--- src/routers/router.js
+++ src/routers/router.js
@@ -5,12 +5,6 @@ const routes = [
         path: '/',
         name: 'BookList',
         component: () => import('../views/BookList.vue')
-    },
-    {
-        path: '/edit-book/:id',
-        name: 'EditBook',
-        props: true,
-        component: () => import('../views/EditBook.vue')
     }]
 
 export const router = createRouter({
