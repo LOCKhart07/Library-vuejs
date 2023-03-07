2fd59c5266608b4f7e2011bfeb0ff05f9518a6c6
Jenslee Dsouza <dsouzajenslee@gmail.com>
2023-03-07 21:12:55 +0530

    Removed unnecessary code clutter and packages

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { router } from './routers/router'
 import { createPinia } from 'pinia'
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
 
 const store = createPinia()
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(store).mount('#app')
